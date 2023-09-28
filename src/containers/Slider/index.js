c92b3ed0ff049a837a22a1bfaacd3026b3cb4a69
Cedric Fournier <cf11157701@gmail.com>
2023-09-28 15:57:18 +0200

    debug slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -12,7 +12,7 @@ const Slider = () => {
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
       5000
     );
   };
@@ -22,7 +22,7 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+
           <div
             key={event.title}
             className={`SlideCard SlideCard--${
@@ -38,20 +38,19 @@ const Slider = () => {
               </div>
             </div>
           </div>
+      ))}
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc?.map((radiob, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${radiob.radioIdx}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
           </div>
-        </>
-      ))}
     </div>
   );
 };
