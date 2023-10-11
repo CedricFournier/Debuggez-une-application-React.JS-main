453591a796fce7e37ce686e0b161f70f8e258d72
CedricFournie <cf11157701@gmail.com>
2023-10-11 21:05:23 +0200

    ajout test 1

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -29,8 +29,9 @@ describe("When Form is created", () => {
 
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    // to implement
+  it("a list of events is displayed", async() => {
+    render(<Home />);
+    await screen.findByText("Catégories");
   })
   it("a list a people is displayed", () => {
     // to implement
